2c0ce53a62f537049d1bb3bd7f8f42a562d8e713
Jayfan111 <1290029424@qq.com>
2026-07-13 01:15:30 +0800

    写了一些main方法

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,17 @@
-public class Game {
-    static void main(){
+import core.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static core.Grid.setBoats;
+import static core.Point.randomSet;
+
+public class Game {
+    static void main() {
+        List<Boat> boats = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            boats.add(new Boat(randomSet()));
+        }
+        setBoats(boats);
     }
 }
--- src/core/Grid.java
+++ src/core/Grid.java
@@ -1,6 +1,12 @@
 package core;
 
+import java.util.List;
+
 public class Grid {
 //    10*10
+    private static List<Boat> Boats;
 
+    public static void setBoats(List<Boat> boats) {
+        Boats = boats;
+    }
 }
